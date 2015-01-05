f8f77fbf261b8b475e35ac8e99adc0c096137592
cysnake4713 <cysnake4713@gmail.com>
2015-01-05 23:37:15 +0800

    [IMP]细节修正

--- addons/ft_project/models/project.py
+++ addons/ft_project/models/project.py
@@ -10,8 +10,30 @@ class ProjectInherit(models.Model):
 
     department_id = fields.Many2one('hr.department', 'Department')
 
+    task_process = fields.Float('Task Process', compute='_compute_task_process')
+
+    @api.multi
+    @api.depends('tasks.stage_id.is_end')
+    def _compute_task_process(self):
+        for project in self:
+            total = len(project.tasks)
+            finished = len(project.tasks.filtered(lambda record: record.stage_id.is_end is True))
+            project.task_process = finished / float(total) * 100 if total else 0
+
 
 class TaskInherit(models.Model):
     _inherit = 'project.task'
 
     department_id = fields.Many2one('hr.department', 'Department')
+
+    @api.multi
+    def write(self, vals):
+        if 'stage_id' in vals and self.env['project.task.type'].browse(vals['stage_id']).is_end is True:
+            vals['date_end'] = fields.Datetime.now()
+        return super(TaskInherit, self).write(vals)
+
+
+class TaskTypeInherit(models.Model):
+    _inherit = 'project.task.type'
+
+    is_end = fields.Boolean('Is End')
