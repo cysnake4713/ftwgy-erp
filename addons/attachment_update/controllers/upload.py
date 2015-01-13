dd2a0e73979a8cb66e806c25bbfaf5e943c2566a
cysnake4713 <cysnake4713@gmail.com>
2015-01-13 14:37:50 +0800

    [IMP] 文档流形式传输,ajax 形式调整

--- addons/attachment_update/controllers/upload.py
+++ addons/attachment_update/controllers/upload.py
@@ -1,8 +1,10 @@
 __author__ = 'cysnake4713'
 
+import simplejson
 from openerp.addons.web.controllers.main import Binary, content_disposition, serialize_exception
 from openerp import http
 from openerp.http import request
+from openerp.tools.translate import _
 
 
 class BinaryExtend(Binary):
@@ -16,7 +18,6 @@ class BinaryExtend(Binary):
                                          [('Content-Type', 'application/octet-stream'),
                                           ('Content-Disposition', content_disposition(filename))])
 
-
     @http.route('/web/binary/saveas', type='http', auth="public")
     @serialize_exception
     def saveas(self, model, field, id=None, filename_field=None, **kw):
@@ -24,3 +25,26 @@ class BinaryExtend(Binary):
             return self.attachment_saveas(model, field, id, filename_field, **kw)
         else:
             return super(BinaryExtend, self).saveas(model, field, id, filename_field, **kw)
+
+    def attachment_saveas_ajax(self, data, token):
+        jdata = simplejson.loads(data)
+        id = jdata.get('id', None)
+        attachment_obj = request.session.model('ir.attachment')
+        if id:
+            filename, outfile = attachment_obj.get_attachment_file(int(id), request.context)
+            return request.make_response(outfile,
+                                         headers=[('Content-Type', 'application/octet-stream'),
+                                                  ('Content-Disposition', content_disposition(filename))],
+                                         cookies={'fileToken': token})
+        else:
+            raise ValueError(_("No attachment found for id '%s'") % id)
+
+
+    @http.route('/web/binary/saveas_ajax', type='http', auth="public")
+    @serialize_exception
+    def saveas_ajax(self, data, token):
+        jdata = simplejson.loads(data)
+        if jdata['model'] == 'ir.attachment':
+            return self.attachment_saveas_ajax(data, token)
+        else:
+            return super(BinaryExtend, self).saveas_ajax(data, token)
