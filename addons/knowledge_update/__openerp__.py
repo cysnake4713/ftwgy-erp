1f779a82edee4d32664f175b0598a6c9febb7490
cysnake4713 <cysnake4713@gmail.com>
2015-01-21 17:14:33 +0800

    [IMP] 知识管理，添加文件列表

---
+++ addons/knowledge_update/__openerp__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Knowledge Update Module',
+    'version': '0.2',
+    'category': 'base',
+    'complexity': "easy",
+    'description': """
+""",
+    'author': 'Matt Cai',
+    'website': 'http://odoosoft.com',
+    'depends': ['base', 'knowledge'],
+    'data': [
+        'views/menu.xml',
+    ],
+    'qweb': [
+        'static/src/xml/*.xml',
+    ],
+    'demo': [],
+    'application': True
+}
