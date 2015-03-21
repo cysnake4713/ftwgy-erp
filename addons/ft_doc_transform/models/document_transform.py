10eacce221272b3e0deb92558f494cd09fc69165
cysnake4713 <cysnake4713@gmail.com>
2015-03-21 22:18:12 +0800

    [IMP]添加直接审批到老师流程

--- addons/ft_doc_transform/models/document_transform.py
+++ addons/ft_doc_transform/models/document_transform.py
@@ -15,14 +15,16 @@ class DocumentTransform(models.Model):
     state = fields.Selection([('draft', 'Draft'),
                               ('principal', 'Principal'),
                               ('department', 'Department'),
-                              ('finish', 'Finish'), ], 'State', default='draft', track_visibility='onchange')
+                              ('finish', 'Finish'), ('finish_teacher', 'Finish By Teacher')], 'State', default='draft', track_visibility='onchange')
     # draft
     attachments = fields.Many2many('ir.attachment', 'rel_ft_doc_attachments', 'doc_id', 'attachment_id', 'Attachments')
     request_principal_user = fields.Many2one('res.users', 'Request Principal User')
     draft_user = fields.Many2one('res.users', 'Draft User')
     draft_datetime = fields.Datetime('Draft Request Datetime')
     # principal
+    principal_apply_type = fields.Selection([('department', 'To Department'), ('teacher', 'To Teacher')], 'Principal To', default='department')
     request_department_users = fields.Many2many('res.users', 'rel_ft_doc_trans_department_users', 'doc_id', 'user_id', 'Request Department Users')
+    request_department_teacher_users = fields.Many2many('res.users', 'rel_ft_doc_trans_cc_users', 'doc_id', 'user_id', 'Request Department CC Users')
     principal_user = fields.Many2one('res.users', 'Principal User')
     principal_datetime = fields.Datetime('Principal Datetime')
     # department
@@ -42,6 +44,15 @@ class DocumentTransform(models.Model):
         'department': True,
         'finish': False,
     }
+
+    @api.multi
+    def principal_apply(self):
+        if self.principal_apply_type == 'teacher':
+            local_context = self.env.context.copy()
+            local_context.update({'state': 'finish_teacher'})
+            return self.with_context(local_context).common_apply()
+        else:
+            return self.common_apply()
 
 
 
